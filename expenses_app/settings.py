54a2764626b9e808a181443d4bbd93d141d39fb6
Ilia Kaziamov <kaziamov@outlook.com>
2023-06-18 15:09:50 +0300

    Fix variables parsing

--- expenses_app/settings.py
+++ expenses_app/settings.py
@@ -1,17 +1,17 @@
 import dotenv
 import os
-from urllib.parse import urlparse
+# from urllib.parse import urlparse
 
 
 dotenv.load_dotenv()
-DATABASE_URL = urlparse(os.getenv('DATABASE_URL'))
 
+DB_NAME = os.getenv("PG_DATABASE", "postgres")
+DB_USER = os.getenv("PG_USER", "postgres")
+DB_PASS = os.getenv("PG_PASSWORD", "postgres")
+DB_HOST = str(os.getenv("PG_HOST", "0.0.0.0"))
+DB_PORT = int(os.getenv("PG_PORT", 5432))
 
-DB_HOST = DATABASE_URL.hostname
-DB_PORT = DATABASE_URL.port
-DB_NAME = DATABASE_URL.path[1:]
-DB_USER = DATABASE_URL.username
-DB_PASS = DATABASE_URL.password
+DATABASE_URL = f'postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 
 MAX_CONN = os.getenv('MAX_CONN', 2)
 MIN_CONN = os.getenv('MAX_CONN', 1)
