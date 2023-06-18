7c596e4583d9c26cbf4a22b0ef33c981da847a4f
Ilia Kaziamov <kaziamov@outlook.com>
2023-06-18 15:10:34 +0300

    Move code to controllers

--- expenses_app/views.py
+++ expenses_app/views.py
@@ -53,12 +53,10 @@ def expenses():
 
 @main.route('/add_new_category', methods=["POST"])
 def add_new_category():
-    with get_connection() as conn:
-        form_values = request.form.to_dict()
-        new_category = form_values['new_category'].strip()
-        add_category(conn, new_category)
-        flash('Категория добавлена', 'success')
-    return redirect(url_for('main.categories', new_category=new_category))
+    form_values = request.form.to_dict()
+    result = controllers.add_new_category(form_values)
+    flash('Категория добавлена', 'success')
+    return redirect(url_for('main.categories', new_category=result))
 
 
 @main.route('/categories', methods=["GET"])
@@ -72,12 +70,10 @@ def categories(new_category=''):
 
 @main.route('/add_new_currency', methods=["POST"])
 def add_new_currency():
-    with get_connection() as conn:
-        form_values = request.form.to_dict()
-        new_currency = form_values['new_currency'].strip()
-        add_currency(conn, new_currency)
-        flash('Валюта добавлена', 'success')
-    return redirect(url_for('main.currencies', new_currency=new_currency))
+    form_values = request.form.to_dict()
+    result = controllers.add_new_currency(form_values)
+    flash('Валюта добавлена', 'success')
+    return redirect(url_for('main.currencies', new_currency=result))
 
 
 @main.route('/currencies', methods=["GET"])
