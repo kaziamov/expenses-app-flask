9b0eb351f88176beac1255587c4bc5c0b00c74f8
Ilia Kaziamov <kaziamov@outlook.com>
2023-06-21 22:55:03 +0300

    Remove default values

--- expenses_app/settings.py
+++ expenses_app/settings.py
@@ -1,15 +1,13 @@
 import dotenv
 import os
-# from urllib.parse import urlparse
-
 
 dotenv.load_dotenv()
 
-DB_NAME = os.getenv("PGDATABASE", "postgres")
-DB_USER = os.getenv("PGUSER", "postgres")
-DB_PASS = os.getenv("PGPASSWORD", "postgres")
-DB_HOST = str(os.getenv("PGHOST", "0.0.0.0"))
-DB_PORT = int(os.getenv("PGPORT", 5432))
+DB_NAME = os.getenv("PGDATABASE")
+DB_USER = os.getenv("PGUSER")
+DB_PASS = os.getenv("PGPASSWORD")
+DB_HOST = os.getenv("PGHOST")
+DB_PORT = os.getenv("PGPORT")
 
 DATABASE_URL = f'postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 
