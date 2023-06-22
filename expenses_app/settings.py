f1b2dc05b71031016bc0bef96657e46576154deb
Ilia Kaziamov <kaziamov@outlook.com>
2023-06-22 09:31:34 +0300

    Update default max_conn

--- expenses_app/settings.py
+++ expenses_app/settings.py
@@ -11,5 +11,5 @@ DB_PORT = os.getenv("PGPORT")
 
 DATABASE_URL = f'postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 
-MAX_CONN = os.getenv('MAX_CONN', 2)
-MIN_CONN = os.getenv('MAX_CONN', 1)
+MAX_CONN = int(os.getenv('MAX_CONN', 2))
+MIN_CONN = int(os.getenv('MAX_CONN', 1))
